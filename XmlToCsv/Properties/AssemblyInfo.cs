34909d5428502d559e2404fafc27d976798f1af9
ITAgnesmeyer <guido.agnesmeyer@ITAgnesmeyer.com>
2024-01-29 07:17:08 +0100

    Upgrade net 8.0

--- XmlToCsv/Properties/AssemblyInfo.cs
+++ XmlToCsv/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("XmlToCsv")]
-[assembly: AssemblyDescription("XmlToCsv")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dipl-. Ing.(FH) Guido Agnesmeyer")]
-[assembly: AssemblyProduct("XmlToCsv")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("XmlToCsv")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("5d40bb9c-fb8a-46fb-9a94-ec15e8552dd2")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
